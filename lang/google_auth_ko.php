7167512b6f3c0a13143a9ae93227078e1d826841
gizmore <gizmore@wechall.net>
2026-09-18 04:04:22 +0200

    'more korean'

---
+++ lang/google_auth_ko.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+namespace GDO\GoogleAuth\lang;
+
+return [
+	'module_googleauth' => '구글 로그인',
+	'link_google_auth' => 'Google로 계속하기',
+	'cfg_google_auth' => 'Google 로그인을 활성화하시겠습니까?',
+	'cfg_google_client_id' => '구글 클라이언트 ID',
+	'cfg_google_client_secret' => 'Google 클라이언트 비밀번호',
+	'cfg_google_redirect_uri' => '구글 콜백 URL',
+	'cfg_google_import_avatar' => 'Google에서 아바타를 업데이트하시겠습니까?',
+	'google_real_name' => 'Google 프로필 이름',
+	'err_google_disabled' => 'Google 로그인이 구성되지 않았습니다.',
+	'err_google_cancelled' => 'Google 로그인이 취소되었습니다.',
+	'err_google_state' => 'Google 로그인 요청이 만료되었거나 유효하지 않습니다.',
+	'err_google_request' => 'Google에 안전하게 연결할 수 없습니다.',
+	'err_google_token' => 'Google에서 사용 가능한 로그인 토큰을 반환하지 않았습니다.',
+	'err_google_profile' => 'Google은 확인된 이메일 신원을 제공하지 않았습니다.',
+	'err_google_identity' => '연결된 Google ID에는 로컬 계정이 없습니다.',
+	'err_google_email' => '둘 이상의 로컬 계정이 이 이메일 주소를 사용합니다.',
+];
